57fe2e88495658f50aaa7795bbcbe9b4c86eddc2
carla kesh <cocokechichian@gmail.com>
2016-05-19 20:12:04 -0700

    logging in menu ..

--- app/src/main/java/com/example/carla/snapchat/LoginMenuFragment.java
+++ app/src/main/java/com/example/carla/snapchat/LoginMenuFragment.java
@@ -9,7 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 
 
 /**
@@ -17,8 +25,11 @@ import android.widget.ListView;
  */
 public class LoginMenuFragment extends Fragment {
 
+    private EditText usernameField;
+    private EditText passwordField;
 
     public LoginMenuFragment() {
+
         // Required empty public constructor
     }
 
@@ -29,35 +40,68 @@ public class LoginMenuFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        Button button = (Button) view.findViewById(R.id.LoginButton);
+        usernameField = (EditText) view.findViewById(R.id.usernameField);
+        passwordField = (EditText) view.findViewById(R.id.passwordField);
 
-        String[] loginMenuItems = {"Register",
-                "Log In"};
-        ListView listView = (ListView)view.findViewById(R.id.loginMenu);
-
-
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                loginMenuItems
-        );
-
-        listView.setAdapter(listViewAdapter);
+        button.setOnClickListener(new View.OnClickListener() {
 
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+      //  String[] loginMenuItems = {"Register",
+      //          "Log In"};
+      //  ListView listView = (ListView)view.findViewById(R.id.loginMenu);
+
+
+     //   ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+       //         getActivity(),
+        //        android.R.layout.simple_list_item_1,
+          //      loginMenuItems );
+
+       // listView.setAdapter(listViewAdapter);
+
+
+       // listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+         //   @Override
+           // public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             //   if (position == 0) {
+               //     Intent intent = new Intent(getActivity(), RegisterActivity.class);
+                 //   startActivity(intent);
+                //} else if (position == 1) {
+                  //  Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    //startActivity(intent);
+                //}
+            //}
+
+        //});
+
+        //return view;
+        //}
+    //}
+
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    Intent intent = new Intent(getActivity(), RegisterActivity.class);
-                    startActivity(intent);
-                } else if (position == 1) {
-                    Intent intent = new Intent(getActivity(), LoginActivity.class);
-                    startActivity(intent);
-                }
+            public void onClick(View v) {
+                String username = usernameField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                Backendless.UserService.login(username, password, new AsyncCallback<BackendlessUser>() {
+                    @Override
+                    public void handleResponse(BackendlessUser response) {
+                        Toast.makeText(getActivity(), "You have logged in!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(getActivity(), "Logging in failed", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
             }
 
-        });
+
+                });
 
         return view;
-        }
     }
+
+}
--- app/src/main/java/com/example/carla/snapchat/RegisterActivity.java
+++ app/src/main/java/com/example/carla/snapchat/RegisterActivity.java
@@ -9,5 +9,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        RegisterFragment register = new RegisterFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.registerContainer, register).commit();
     }
 }
