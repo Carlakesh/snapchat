14d65604555b834e7995c41a56d6b108148216ae
carla kesh <cocokechichian@gmail.com>
2016-05-22 22:21:00 -0700

    ,

---
+++ app/src/main/java/com/example/carla/snapchat/LoggingoutFragment.java
@@ -0,0 +1,61 @@
+package com.example.carla.snapchat;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.example.carla.snapchat.MainActivity;
+import com.example.carla.snapchat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoggingoutFragment extends Fragment {
+
+
+    public LoggingoutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_loggingout2, container, false);
+
+        Button logOutButton = (Button) view.findViewById(R.id.LogOutButton);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Backendless.UserService.logout(new AsyncCallback<Void>() {
+                    @Override
+                    public void handleResponse(Void response) {
+                        Toast.makeText(getActivity(), "You have logged out!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(getActivity(), "Failed to log out..", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+                    return view;
+            }
+
+        });
+
+    }
+
